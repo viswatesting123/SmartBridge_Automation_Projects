caf446ae5d85ad088921c68cf6c7dd74e4c521e0
viswanadh <viswanadh.m1105@gmail.com>
2023-07-10 11:31:23 +0530

    ideaboardz project

---
+++ ideaboardz automation/Object Repository/Page_IdeaBoardz - Brainstorm, Retrospect, C_9c164f/li_Add ideas at any time.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>li_Add ideas at any time</name>
+   <tag></tag>
+   <elementGuidId>64eb65d8-4d81-4f66-82ff-b41e906385e7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.why.orange.sideNote > div.sectionContent > ul > li</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Why?'])[1]/following::li[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>li</value>
+      <webElementGuid>7031a481-34b1-4ccd-9061-d106a2dc93df</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Add ideas at any time.</value>
+      <webElementGuid>60ba7c01-8bb9-468d-b078-2c6b1df0f7b0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;mainWrap&quot;]/div[@class=&quot;whiteboard&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;rightBar half left&quot;]/div[@class=&quot;why orange sideNote&quot;]/div[@class=&quot;sectionContent&quot;]/ul[1]/li[1]</value>
+      <webElementGuid>0842a772-eb36-437a-a5a0-831c364f48e7</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Why?'])[1]/following::li[1]</value>
+      <webElementGuid>144478a8-65d2-4291-a732-c7873a0d3ca3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terms and Conditions.'])[1]/following::li[1]</value>
+      <webElementGuid>da083008-ca1e-4c93-9398-eed839a4e86a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Brainstorm across oceans.'])[1]/preceding::li[1]</value>
+      <webElementGuid>6ea6cd41-8141-4ecb-b59e-1c9ba6bae828</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Export as PDF or Xls.'])[1]/preceding::li[2]</value>
+      <webElementGuid>4dbf8e74-46ab-46c9-bc94-1bf08e663478</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Add ideas at any time.']/parent::*</value>
+      <webElementGuid>5b07999d-6415-4397-b681-c5035656ef4a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/ul/li</value>
+      <webElementGuid>57b4d26c-cc8a-4e9a-a5ff-8978195266ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//li[(text() = 'Add ideas at any time.' or . = 'Add ideas at any time.')]</value>
+      <webElementGuid>0de52530-2468-4c42-b13d-4eae3c98c747</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ ideaboardz automation/Test Suites/Ideal Boardz Test Suite.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Ideal Boardz Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5ef212db-8a61-43fe-95c9-3b4caa4e53cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>cbdd0036-2c73-4eaa-a84d-6f289cff1133</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test/Navigate Login page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7dfba7ba-d1f2-4d50-b7ed-c0b858ca326b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test/Validate Home Page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
